000707fc0dc74e1a36df52c87d447c5e9c344380
katherine <137005702+katherine-shilenko@users.noreply.github.com>
2025-11-23 11:00:31 -0500

    Implement keyword sorting

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import os
 import shutil
 
-directory_to_sort = "Random Folder"   # <--- UPDATE WITH THE FOLDER NAME YOU'D LIKE TO HAVE SORTED
-directory = os.path.join(os.path.expanduser("~"), directory_to_sort)
+directory = r""    # <--- UPDATE WITH THE DIRECTORY PATH YOU'D LIKE TO HAVE SORTED
+
+# UPDATE WITH THE KEYWORD YOU'D LIKE TO USE FOR SORTING
+# Example file: my_dog.jpg -> keyword = "dog"
+# Leave empty to match all files
+keyword = ""
+
+# IF SORTING BY KEYWORD, ADD NEW FOLDER NAME HERE
+# Otherwise, use extensions below
+new_folder = ""
 
 # ADD EXTENSIONS AS NEEDED
 # Format: ".extension": "Desired Folder Name"
@@ -23,8 +31,8 @@ extensions = {
     ".csv": "Excel Sheets",
     ".xlsx": "Excel Sheets",
     ".exe": "Executables",
-    ".js": "Java Script",
-    ".mjs": "Java Script",
+    ".js": "JavaScript",
+    ".mjs": "JavaScript",
     ".css": "CSS"
 }
 
@@ -33,19 +41,23 @@ try:
         file_path = os.path.join(directory, filename)
         if os.path.isfile(file_path):
             extension = os.path.splitext(filename)[1].lower()
-            if extension in extensions:
-                folder_name = extensions[extension]
+            if extension in extensions and keyword in filename:
+                if new_folder == "":
+                    folder_name = extensions[extension]
+                else:
+                    folder_name = new_folder
                 folder_path = os.path.join(directory, folder_name)
                 os.makedirs(folder_path, exist_ok=True)
                 destination_path = os.path.join(folder_path, filename)
                 shutil.move(file_path, destination_path)
                 print(f"Moved {filename} to {folder_name} folder.")
             else:
-                print(f"Skipped {filename}. Unknown file extension.")
+                print(f"Skipped {filename}. Unknown file extension or keyword.")
+
         else:
             print(f"Skipped {filename}. It is a directory.")
-        print("File organization completed.")
+
+    print("File organization completed.")
 
 except OSError as e:
     print(f"Path not found: {e.filename}")
-
