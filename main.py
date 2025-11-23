14c37f406c284e7df0b05008945f94930aeb9e8d
katherine <137005702+katherine-shilenko@users.noreply.github.com>
2025-11-23 10:41:32 -0500

    Add error handling

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 import shutil
 
-directory = os.path.join(os.path.expanduser("~"), "Desktop")
+directory_to_sort = "Random Folder"   # <--- UPDATE WITH THE FOLDER NAME YOU'D LIKE TO HAVE SORTED
+directory = os.path.join(os.path.expanduser("~"), directory_to_sort)
 
+# ADD EXTENSIONS AS NEEDED
+# Format: ".extension": "Desired Folder Name"
 extensions = {
     ".jpg": "Images",
     ".png": "Images",
@@ -20,25 +23,29 @@ extensions = {
     ".csv": "Excel Sheets",
     ".xlsx": "Excel Sheets",
     ".exe": "Executables",
-    ".js": "Coding",
-    ".mjs": "Coding",
-    ".css": "Coding"
+    ".js": "Java Script",
+    ".mjs": "Java Script",
+    ".css": "CSS"
 }
 
-for filename in os.listdir(directory):
-    file_path = os.path.join(directory, filename)
-    if os.path.isfile(file_path):
-        extension = os.path.splitext(filename)[1].lower()
-        if extension in extensions:
-            folder_name = extensions[extension]
-            folder_path = os.path.join(directory, folder_name)
-            os.makedirs(folder_path, exist_ok=True)
-            destination_path = os.path.join(folder_path, filename)
-            shutil.move(file_path, destination_path)
-            print(f"Moved {filename} to {folder_name} folder.")
+try:
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if os.path.isfile(file_path):
+            extension = os.path.splitext(filename)[1].lower()
+            if extension in extensions:
+                folder_name = extensions[extension]
+                folder_path = os.path.join(directory, folder_name)
+                os.makedirs(folder_path, exist_ok=True)
+                destination_path = os.path.join(folder_path, filename)
+                shutil.move(file_path, destination_path)
+                print(f"Moved {filename} to {folder_name} folder.")
+            else:
+                print(f"Skipped {filename}. Unknown file extension.")
         else:
-            print(f"Skipped {filename}. Unknown file extension.")
-    else:
-        print(f"Skipped {filename}. It is a directory.")
+            print(f"Skipped {filename}. It is a directory.")
+        print("File organization completed.")
 
-print("File organization completed.")
+except OSError as e:
+    print(f"Path not found: {e.filename}")
+
